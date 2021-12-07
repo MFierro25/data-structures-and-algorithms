f5e8088d61421588f8408ac96608d2f4946dc145
Miguel Fierro <emfierro2@gmail.com>
2021-12-06 23:17:07 -0800

    created append, prepend and insert methods

---
+++ python/code_challenges/linked-list-insertions/README.md
@@ -0,0 +1,14 @@
+# Challenge Summary
+<!-- Description of the challenge -->
+Write methods for a Linked list.
+These methods include, Append, Insert before and insert after. Test each method that it is working properly
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+![Linked List Insertion Whiteboard](assets/linked-list-whiteboard.png)
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
--- python/code_challenges/linked-list-insertions/tests/test_linked_list_insertions.py
+++ python/code_challenges/linked-list-insertions/tests/test_linked_list_insertions.py
@@ -1,5 +1,28 @@
-from linked_list_insertions import __version__
+
+from linked_list_insertions.linked_list_insertions import LinkedList
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+## succesfully insert node to empty list
+def test_node_inserts_to_head():
+    lst = LinkedList()
+    lst.append('first')
+    assert lst.head.value == 'first'
+    
+# Can successfully add a node to the end of the linked list
+
+# def test_append_to_end():
+#     lst = LinkedList
+#     lst.insert('four')
+#     lst.append('five')
+#     assert lst.head.value == 'four'
+#     assert lst.head.next.value == 'five'
+    
+# Can successfully add multiple nodes to the end of a linked list
+
+# Can successfully insert a node before a node located in the middle of a linked list
+
+# Can successfully insert a node before the first node of a linked list
+
+# Can successfully insert after a node in the middle of the linked list
+
+# Can successfully insert a node after the last node of the linked list
