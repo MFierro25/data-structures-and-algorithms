340d28854f8874ef5481ab64630e6aba4d74ca88
Miguel Fierro <emfierro2@gmail.com>
2021-12-07 17:41:36 -0800

    completed all methods and test for insertion

--- python/code_challenges/linked-list-insertions/tests/test_linked_list_insertions.py
+++ python/code_challenges/linked-list-insertions/tests/test_linked_list_insertions.py
@@ -10,19 +10,101 @@ def test_node_inserts_to_head():
     
 # Can successfully add a node to the end of the linked list
 
-# def test_append_to_end():
-#     lst = LinkedList
-#     lst.insert('four')
-#     lst.append('five')
-#     assert lst.head.value == 'four'
-#     assert lst.head.next.value == 'five'
+def test_append_to_end():
+    lst = LinkedList()
+    lst.insert('four')
+    lst.append('five')
+    actual = lst.head.value
+    expected = 'four'
+    assert actual == expected
+    
+    actual = lst.head.next.value
+    expected = 'five'
+    
     
 # Can successfully add multiple nodes to the end of a linked list
 
+def test_append_multiple():
+    lst = LinkedList()
+    lst.insert('four')
+    lst.append('five')
+    lst.append('six')
+    actual = lst.head.value
+    expected = 'four'
+    assert actual == expected
+    actual = lst.head.next.value
+    expected = 'five'
+    
+    actual = lst.head.next.next.value
+    expected = 'six'
+    
+    
 # Can successfully insert a node before a node located in the middle of a linked list
 
-# Can successfully insert a node before the first node of a linked list
+def test_insert_middle_before():
+    lst = LinkedList()
+    lst.insert('four')
+    lst.append('five')
+    lst.insert_before('five', 'ten')
+    actual = lst.head.value
+    expected = 'four'
+    assert actual == expected
+    actual = lst.head.next.value
+    expected = 'ten'
+    
+    actual = lst.head.next.next.value
+    expected = 'five'
+   
 
+# Can successfully insert a node before the first node of a linked list
+def test_prepend():
+    lst =LinkedList()
+    lst.insert('four')
+    lst.prepend('three')
+    actual = lst.head.value
+    expected = 'three'
+    
+    actual = lst.head.next.value
+    expected = 'four'
+    
+    assert actual == expected
+    
 # Can successfully insert after a node in the middle of the linked list
 
+def test_insert_middle_after():
+    lst = LinkedList()
+    lst.insert('four')
+    lst.append('five')
+    lst.append('six')
+    lst.insert_after('five', 'ten')
+    actual = lst.head.value
+    expected = 'four'
+    assert actual == expected
+    actual = lst.head.next.value
+    expected = 'five'
+    
+    actual = lst.head.next.next.value
+    expected = 'ten'
+    
+    actual = lst.head.next.next.next.value
+    expected = 'six'
+
 # Can successfully insert a node after the last node of the linked list
+
+def test_insert_end():
+    lst = LinkedList()
+    lst.insert('four')
+    lst.append('five')
+    lst.append('six')
+    lst.insert_after('six', 'ten')
+    actual = lst.head.value
+    expected = 'four'
+    assert actual == expected
+    actual = lst.head.next.value
+    expected = 'five'
+    
+    actual = lst.head.next.next.value
+    expected = 'six'
+    
+    actual = lst.head.next.next.next.value
+    expected = 'ten'
